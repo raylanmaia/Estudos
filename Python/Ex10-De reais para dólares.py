2a6685326152ed1ae1e0daca5f84cb8c1160de6f
Raylan Maia <maia.raylan@gmail.com>
2023-12-21 16:27:50 -0300

    exercícios

---
+++ Python/Ex10-De reais para dólares.py
@@ -0,0 +1,4 @@
+reais = float(input("Quantos reais você tem?"))
+dolares = float(3.37)
+em_dolares = round(reais/dolares, 2)
+print ("Você tem",em_dolares,"dólares")
---
+++ Python/Ex8-Conversor de medidas.py
@@ -0,0 +1,10 @@
+metros = float(input("Digite o tamanho em metros para converter:"))
+print (metros//10)
+print (metros//100)
+print (metros//1000)
+print (metros)
+print (metros*10)
+print (metros*100)
+print (metros*1000)
+
+
