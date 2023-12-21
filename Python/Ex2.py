23f1626517a07ec458adcc326c58706b5af6e2db
Raylan Maia <maia.raylan@gmail.com>
2023-12-20 21:46:47 -0300

    testando

---
+++ Python/Ex2.py
@@ -0,0 +1,2 @@
+atendente=input("Qual seu nome?")
+print ("É um prazer te conhecer",atendente)
---
+++ Python/Ex3.py
@@ -0,0 +1,3 @@
+primeiro_numero=int(input("Digite o primeiro número:"))
+segundo_numero=int(input("Digite o segundo número:"))
+print("A soma dos dois números digitados é:", primeiro_numero + segundo_numero)
