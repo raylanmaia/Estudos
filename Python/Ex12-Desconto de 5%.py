c27c90d090e89c68ce7a90cd273aad30d68b6723
Raylan Maia <maia.raylan@gmail.com>
2023-12-22 23:31:27 -0300

    Exercícios 22/12

---
+++ Python/Ex12-Desconto de 5%.py
@@ -0,0 +1,3 @@
+valor_original = float(input("Digite o valor original do produto:"))
+valor_desconto = valor_original * (95/100)
+print ("O valor original do produto é",valor_original,", e com desconto de 5% fica:",valor_desconto)
---
+++ Python/Ex15-Aluguel de carros.py
@@ -0,0 +1,4 @@
+diária = int(input("Quantos dias você usou o carro?"))
+distância = int(input("Quantos KM você rodou no carro?"))
+
+print ("Você tem que pagar",diária*60,"reais e",distância*0.15,"reais pela distância percorrida, dando o total de:",diária*60+distância*0.15,"reais")
