f95f560ca26cc6e75def13e7462175a981944c37
Raylan Maia <maia.raylan@gmail.com>
2023-12-26 19:16:17 -0300

    Exercícios 26/12

---
+++ Python/Ex0037.py
@@ -0,0 +1,7 @@
+escolha = int(input("Escolha: 1 (binário), 2 (octal) e 3 (hexadecimal)!:"))
+if escolha == 1:
+    print("Você escolheu binário!")
+elif escolha == 2:
+    print("Você escolheu octal!")
+elif escolha == 3:
+    print("Você escolheu hexadecimal!")
